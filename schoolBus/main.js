80c9253e4dde37360b2af2abea204a45aa7aa055
fkks33 <97004922+fkks33@users.noreply.github.com>
2022-04-12 08:16:50 +0900

    Add files via upload

--- schoolBus/main.js
+++ schoolBus/main.js
@@ -8,80 +8,10 @@ const dia_fo_2 = [630, 690, 755];
 const dia_of_2 = [650, 710, 775];
 const dia_fs_2 = [600, 720, 795, 900];
 const dia_sf_2 = [660, 780, 840, 1020];
-let wed_fo; //水曜日用
+let dia_fo_wednesday; //水曜日用
 
-//ダイヤ検査用
-function diaCheck(d) {
-    for(let i = 0; i < d.length; i++) {
-        let h = d[i] / 60;
-        h = Math.floor(h);
-        let m = d[i] - (h * 60);
-        console.log(`${h} : ${m}`);
-    }
-}
-
-//現在時刻を分に換算して返す
-function getCurrent_M() {
-    let d = new Date();
-    let h = d.getHours();
-    let m = d.getMinutes();
-    let t = h * 60 + m;
-    // console.log("経過時間（分）：" + t);
-    return t;
-}
-
-//現在時刻を秒に換算して返す
-function getCurrent_S() {
-    let d = new Date();
-    let h = d.getHours();
-    let m = d.getMinutes();
-    let s = d.getSeconds();
-    let t =  h * 3600 + m * 60 + s;
-    // console.log("経過時間（秒）：" + t);
-    return t;
-}
-
-//パターン
-function pattern(p) {
-    document.getElementById("diaPattern").innerHTML = `${p}`;
-}
-
-//「次のバス」を表示（引数 : 現在時刻, ダイヤ, 表示先divのid）
-function next(time, dia, id) {
-    for(let i = 0; i < dia.length; i++) {
-        if(time < dia[i]) {
-            let h = dia[i] / 60;
-            h = Math.floor(h);
-            let m = dia[i] - (h * 60);
-            h = addZero(h);
-            m = addZero(m);
-            document.getElementById(id).innerHTML = `${h} : ${m}`;
-            break;
-        } else {
-            document.getElementById(id).innerHTML = `☆本日の運転は終了しました☆`;
-        }
-    }
-}
-
-//「残り時間」を表示
-function remain(time, dia, id) {
-    for(let i = 0; i < dia.length; i++) {
-        if(time < dia[i] * 60) {
-            let t = dia[i] * 60 - time; //tは残り時間（秒）
-            let h = t / 3600;
-            h = Math.floor(h);
-            let m = (t - 3600 * h) / 60;
-            m = Math.floor(m);
-            let s = t - 3600 * h - 60 * m;
-            h = addZero(h);
-            m = addZero(m);
-            s = addZero(s);
-            document.getElementById(id).innerHTML = `${h}h ${m}m ${s}s`;
-            break;
-        } else {
-            document.getElementById(id).innerHTML = ``;
-        }
-    }
+function write(id, str) {
+    document.getElementById(id).innerHTML = str;
 }
 
 //引数が0~9なら先頭に0をつけてStringで返す
@@ -95,64 +25,205 @@ function addZero(n) {
     return s;
 }
 
+//現在時刻を秒に換算して返す
+function getCurrentTime() {
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let t =  h * 3600 + m * 60 + s;
+    return t;
+}
+
+//秒表記の時間を「hhmmss」にする
+function hhmmss(sec) {
+    let h = sec / 3600;
+    h = Math.floor(h);
+    h = addZero(h);
+    let m = (sec - 3600 * h) / 60;
+    m = Math.floor(m);
+    m = addZero(m);
+    let s = sec - 3600 * h - 60 * m;
+    s = addZero(s);
+    let t = `${h} : ${m} : ${s}`;
+    return t;
+}
+
+//引数は（現在時刻（秒）, 基準ダイヤ, 発順（0が先発で））
+function getNext(now, dia, order) {
+    let msg;
+    //現在時刻を分に変換
+    let h = now / 3600;
+    h = Math.floor(h);
+    let m = (now - 3600 * h) / 60;
+    m = Math.floor(m);
+    let t = 60 * h + m;
+    //現在時刻をダイヤと照らし合わせ
+    for(let i = 0; i < dia.length; i++) {
+        if(i + order < dia.length && t < dia[i]) { //配列の外を参照しないようにチェック
+            let h2 = dia[i + order] / 60;
+            h2 = Math.floor(h2);
+            h2 = addZero(h2);
+            let m2 = dia[i + order] - h2 * 60;
+            m2 = addZero(m2);
+            msg = `${h2} : ${m2}`;
+            break;
+        } else {
+            if(i + order < dia.length) {
+                msg = "☆本日の運転は終了しました☆";
+            } else {
+                msg = "";
+            }
+            break;
+        }
+    }
+    return msg;
+}
+
+function getRemain(now, dia, order) {
+    let str;
+    for(let i = 0; i < dia.length; i++) {
+        if(i + order < dia.length && now / 60 < dia[i]) {
+            let t = dia[i + order] * 60 - now;
+            let h = t / 3600;
+            h = Math.floor(h);
+            let m = (t - 3600 * h) / 60;
+            m = Math.floor(m);
+            let s = t - 3600 * h - 60 * m;
+            str = `${h}h ${m}m ${s}s`;
+            break;
+        } else {
+            str = "";
+            break;
+        }
+    }
+    return str;
+}
+
 //繰り返す動作
 const weekday = function() {
-    next(getCurrent_M(), dia_fo_1, "foNext");
-    remain(getCurrent_S(), dia_fo_1, "foRemain");
-    next(getCurrent_M(), dia_of_1, "ofNext");
-    remain(getCurrent_S(), dia_of_1, "ofRemain");
-    next(getCurrent_M(), dia_fs_1, "fsNext");
-    remain(getCurrent_S(), dia_fs_1, "fsRemain");
-    next(getCurrent_M(), dia_sf_1, "sfNext");
-    remain(getCurrent_S(), dia_sf_1, "sfRemain");
+    currentTime = getCurrentTime();
+    write("currentTimeDiv", hhmmss(currentTime));
+    //深草大宮
+    write("FukaOmiya_1st_Departure", getNext(currentTime, dia_fo_1, 0));
+    write("FukaOmiya_2nd_Departure", getNext(currentTime, dia_fo_1, 1));
+    write("FukaOmiya_3rd_Departure", getNext(currentTime, dia_fo_1, 2));
+    write("FukaOmiya_1st_Remain", getRemain(currentTime, dia_fo_1, 0));
+    write("FukaOmiya_2nd_Remain", getRemain(currentTime, dia_fo_1, 1));
+    write("FukaOmiya_3rd_Remain", getRemain(currentTime, dia_fo_1, 2));
+    //大宮深草
+    write("OmiyaFuka_1st_Departure", getNext(currentTime, dia_of_1, 0));
+    write("OmiyaFuka_2nd_Departure", getNext(currentTime, dia_of_1, 1));
+    write("OmiyaFuka_3rd_Departure", getNext(currentTime, dia_of_1, 2));
+    write("OmiyaFuka_1st_Remain", getRemain(currentTime, dia_of_1, 0));
+    write("OmiyaFuka_2nd_Remain", getRemain(currentTime, dia_of_1, 1));
+    write("OmiyaFuka_3rd_Remain", getRemain(currentTime, dia_of_1, 2));
+    //深草瀬田
+    write("FukaSeta_1st_Departure", getNext(currentTime, dia_fs_1, 0));
+    write("FukaSeta_2nd_Departure", getNext(currentTime, dia_fs_1, 1));
+    write("FukaSeta_3rd_Departure", getNext(currentTime, dia_fs_1, 2));
+    write("FukaSeta_1st_Remain", getRemain(currentTime, dia_fs_1, 0));
+    write("FukaSeta_2nd_Remain", getRemain(currentTime, dia_fs_1, 1));
+    write("FukaSeta_3rd_Remain", getRemain(currentTime, dia_fs_1, 2));
+    //瀬田深草
+    write("SetaFuka_1st_Departure", getNext(currentTime, dia_sf_1, 0));
+    write("SetaFuka_2nd_Departure", getNext(currentTime, dia_sf_1, 1));
+    write("SetaFuka_3rd_Departure", getNext(currentTime, dia_sf_1, 2));
+    write("SetaFuka_1st_Remain", getRemain(currentTime, dia_sf_1, 0));
+    write("SetaFuka_2nd_Remain", getRemain(currentTime, dia_sf_1, 1));
+    write("SetaFuka_3rd_Remain", getRemain(currentTime, dia_sf_1, 2));
 }
 
 const wednesday = function() {
-    next(getCurrent_M(), wed_fo, "foNext");
-    remain(getCurrent_S(), wed_fo, "foRemain");
-    next(getCurrent_M(), dia_of_1, "ofNext");
-    remain(getCurrent_S(), dia_of_1, "ofRemain");
-    next(getCurrent_M(), dia_fs_1, "fsNext");
-    remain(getCurrent_S(), dia_fs_1, "fsRemain");
-    next(getCurrent_M(), dia_sf_1, "sfNext");
-    remain(getCurrent_S(), dia_sf_1, "sfRemain");
+    currentTime = getCurrentTime();
+    write("currentTimeDiv", hhmmss(currentTime));
+    //深草大宮
+    write("FukaOmiya_1st_Departure", getNext(currentTime, dia_fo_wednesday, 0));
+    write("FukaOmiya_2nd_Departure", getNext(currentTime, dia_fo_wednesday, 1));
+    write("FukaOmiya_3rd_Departure", getNext(currentTime, dia_fo_wednesday, 2));
+    write("FukaOmiya_1st_Remain", getRemain(currentTime, dia_fo_wednesday, 0));
+    write("FukaOmiya_2nd_Remain", getRemain(currentTime, dia_fo_wednesday, 1));
+    write("FukaOmiya_3rd_Remain", getRemain(currentTime, dia_fo_wednesday, 2));
+    //大宮深草
+    write("OmiyaFuka_1st_Departure", getNext(currentTime, dia_of_1, 0));
+    write("OmiyaFuka_2nd_Departure", getNext(currentTime, dia_of_1, 1));
+    write("OmiyaFuka_3rd_Departure", getNext(currentTime, dia_of_1, 2));
+    write("OmiyaFuka_1st_Remain", getRemain(currentTime, dia_of_1, 0));
+    write("OmiyaFuka_2nd_Remain", getRemain(currentTime, dia_of_1, 1));
+    write("OmiyaFuka_3rd_Remain", getRemain(currentTime, dia_of_1, 2));
+    //深草瀬田
+    write("FukaSeta_1st_Departure", getNext(currentTime, dia_fs_1, 0));
+    write("FukaSeta_2nd_Departure", getNext(currentTime, dia_fs_1, 1));
+    write("FukaSeta_3rd_Departure", getNext(currentTime, dia_fs_1, 2));
+    write("FukaSeta_1st_Remain", getRemain(currentTime, dia_fs_1, 0));
+    write("FukaSeta_2nd_Remain", getRemain(currentTime, dia_fs_1, 1));
+    write("FukaSeta_3rd_Remain", getRemain(currentTime, dia_fs_1, 2));
+    //瀬田深草
+    write("SetaFuka_1st_Departure", getNext(currentTime, dia_sf_1, 0));
+    write("SetaFuka_2nd_Departure", getNext(currentTime, dia_sf_1, 1));
+    write("SetaFuka_3rd_Departure", getNext(currentTime, dia_sf_1, 2));
+    write("SetaFuka_1st_Remain", getRemain(currentTime, dia_sf_1, 0));
+    write("SetaFuka_2nd_Remain", getRemain(currentTime, dia_sf_1, 1));
+    write("SetaFuka_3rd_Remain", getRemain(currentTime, dia_sf_1, 2));
 }
 
 const saturday = function() {
-    next(getCurrent_M(), dia_fo_2, "foNext");
-    remain(getCurrent_S(), dia_fo_2, "foRemain");
-    next(getCurrent_M(), dia_of_2, "ofNext");
-    remain(getCurrent_S(), dia_of_2, "ofRemain");
-    next(getCurrent_M(), dia_fs_2, "fsNext");
-    remain(getCurrent_S(), dia_fs_2, "fsRemain");
-    next(getCurrent_M(), dia_sf_2, "sfNext");
-    remain(getCurrent_S(), dia_sf_2, "sfRemain");
+    currentTime = getCurrentTime();
+    write("currentTimeDiv", hhmmss(currentTime));
+    //深草大宮
+    write("FukaOmiya_1st_Departure", getNext(currentTime, dia_fo_2, 0));
+    write("FukaOmiya_2nd_Departure", getNext(currentTime, dia_fo_2, 1));
+    write("FukaOmiya_3rd_Departure", getNext(currentTime, dia_fo_2, 2));
+    write("FukaOmiya_1st_Remain", getRemain(currentTime, dia_fo_2, 0));
+    write("FukaOmiya_2nd_Remain", getRemain(currentTime, dia_fo_2, 1));
+    write("FukaOmiya_3rd_Remain", getRemain(currentTime, dia_fo_2, 2));
+    //大宮深草
+    write("OmiyaFuka_1st_Departure", getNext(currentTime, dia_of_2, 0));
+    write("OmiyaFuka_2nd_Departure", getNext(currentTime, dia_of_2, 1));
+    write("OmiyaFuka_3rd_Departure", getNext(currentTime, dia_of_2, 2));
+    write("OmiyaFuka_1st_Remain", getRemain(currentTime, dia_of_2, 0));
+    write("OmiyaFuka_2nd_Remain", getRemain(currentTime, dia_of_2, 1));
+    write("OmiyaFuka_3rd_Remain", getRemain(currentTime, dia_of_2, 2));
+    //深草瀬田
+    write("FukaSeta_1st_Departure", getNext(currentTime, dia_fs_2, 0));
+    write("FukaSeta_2nd_Departure", getNext(currentTime, dia_fs_2, 1));
+    write("FukaSeta_3rd_Departure", getNext(currentTime, dia_fs_2, 2));
+    write("FukaSeta_1st_Remain", getRemain(currentTime, dia_fs_2, 0));
+    write("FukaSeta_2nd_Remain", getRemain(currentTime, dia_fs_2, 1));
+    write("FukaSeta_3rd_Remain", getRemain(currentTime, dia_fs_2, 2));
+    //瀬田深草
+    write("SetaFuka_1st_Departure", getNext(currentTime, dia_sf_2, 0));
+    write("SetaFuka_2nd_Departure", getNext(currentTime, dia_sf_2, 1));
+    write("SetaFuka_3rd_Departure", getNext(currentTime, dia_sf_2, 2));
+    write("SetaFuka_1st_Remain", getRemain(currentTime, dia_sf_2, 0));
+    write("SetaFuka_2nd_Remain", getRemain(currentTime, dia_sf_2, 1));
+    write("SetaFuka_3rd_Remain", getRemain(currentTime, dia_sf_2, 2));
 }
 
 //main
+let currentTime;
 let d = new Date();
 let day = d.getDay(); //曜日
 switch (day) {
     case 0:
         //日曜日
-        pattern("日曜日（運休）");
+        write("pattern", "日曜日（運休）");
         break;
     case 3:
         //水曜日
-        pattern("平日ダイヤ（水）");
-        wed_fo = dia_fo_1;
-        wed_fo.unshift(525);
-        diaCheck(wed_fo); 
+        write("pattern", "平日ダイヤ（水）");
+        dia_fo_wednesday = dia_fo_1;
+        dia_fo_wednesday.unshift(525);
         setInterval(wednesday, 1000);
         break;
     case 6:
         //土曜日
-        pattern("土曜日ダイヤ");
+        write("pattern", "土曜日ダイヤ");
         setInterval(saturday, 1000);
         break;
     default:
         //水曜日除く平日
-        pattern("平日ダイヤ");
+        write("pattern", "平日ダイヤ");
         setInterval(weekday, 1000);
         break;
 }
